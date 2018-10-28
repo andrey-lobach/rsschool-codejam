98d00bedae755a57ad129f32abfcd80a532afa74
Andrei Lobach <andreylobach1997@gmail.com>
2018-10-28 16:03:59 +0300

    feat: add test for recursion

--- test.js
+++ test.js
@@ -35,3 +35,25 @@ describe('make:', () => {
     assert.deepEqual(test, 982);
   });
 });
+const recursion = require('./tasks/recursion.js');
+
+describe('recursion:', () => {
+  it('1', () => {
+    const test = recursion({ value: 100,
+      left: { value: 90, left: { value: 70 }, right: { value: 99 } },
+      right: { value: 120, left: { value: 110 }, right: { value: 130 } } });
+    assert.deepEqual(test, [[100], [90, 120], [70, 99, 110, 130]]);
+  });
+
+  it('2', () => {
+    const test = recursion({ value: 35, left: { value: 88 }, right: { value: 231 } });
+    assert.deepEqual(test, [[35], [88, 231]]);
+  });
+
+  it('3', () => {
+    const test = recursion({ value: 100,
+      left: { value: 32, left: { value: 12 }, right: { value: 90 } },
+      right: { value: 13, left: { value: 29 }, right: { value: 36 } } });
+    assert.deepEqual(test, [[32], [12, 13], [12, 90, 29, 36]]);
+  });
+});
